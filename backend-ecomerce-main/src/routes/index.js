67102021ef25d39cbd94d9680c7de00926dd88ff
onwen333 <binpro015@gmail.com>
2025-12-31 11:50:29 +0700

    FIX BACKEND

--- backend-ecomerce-main/src/routes/index.js
+++ backend-ecomerce-main/src/routes/index.js
@@ -1,13 +1,19 @@
-const UserRouter = require('./UserRouter')
-const ProductRouter = require('./ProductRouter')
-const OrderRouter = require('./OrderRouter')
-const PaymentRouter = require('./PaymentRouter')
+const UserRouter = require("./UserRouter");
+const ProductRouter = require("./ProductRouter");
+const OrderRouter = require("./OrderRouter");
+const PaymentRouter = require("./PaymentRouter");
 
 const routes = (app) => {
-    app.use('/api/user', UserRouter)
-    app.use('/api/product', ProductRouter)
-    app.use('/api/order', OrderRouter)
-    app.use('/api/payment', PaymentRouter)
-}
+  app.get("/api", (req, res) => {
+    res.status(200).json({
+      status: "OK",
+      message: "Backend is running",
+    });
+  });
+  app.use("/api/user", UserRouter);
+  app.use("/api/product", ProductRouter);
+  app.use("/api/order", OrderRouter);
+  app.use("/api/payment", PaymentRouter);
+};
 
-module.exports = routes
+module.exports = routes;
