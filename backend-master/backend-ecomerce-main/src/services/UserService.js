1fc30296a5c72f107789b76377a51fe6a9164f4b
onwen333 <binpro015@gmail.com>
2025-12-31 22:39:08 +0700

    backend backupp

---
+++ backend-master/backend-ecomerce-main/src/services/UserService.js
@@ -0,0 +1,189 @@
+const User = require("../models/UserModel");
+const bcrypt = require("bcrypt");
+const { generalAccessToken, generalRefreshToken } = require("./JwtService");
+
+const createUser = (newUser) => {
+  return new Promise(async (resolve, reject) => {
+    const { name, email, password, phone } = newUser;
+    try {
+      const checkUser = await User.findOne({
+        email: email,
+      });
+      if (checkUser !== null) {
+        return resolve({
+          status: "ERR",
+          message: "The email is already",
+        });
+      }
+      const hash = bcrypt.hashSync(password, 10);
+      const createdUser = await User.create({
+        name,
+        email,
+        password: hash,
+        phone,
+      });
+      if (createdUser) {
+        resolve({
+          status: "OK",
+          message: "SUCCESS",
+          data: createdUser,
+        });
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+const loginUser = (userLogin) => {
+  return new Promise(async (resolve, reject) => {
+    const { email, password } = userLogin;
+    try {
+      const checkUser = await User.findOne({
+        email: email,
+      });
+
+      if (checkUser === null) {
+        return resolve({
+          status: "ERR",
+          message: "The user is not defined",
+        });
+      }
+
+      const comparePassword = bcrypt.compareSync(password, checkUser.password);
+
+      if (!comparePassword) {
+        return resolve({
+          status: "ERR",
+          message: "The password or user is incorrect",
+        });
+      }
+
+      const access_token = await generalAccessToken({
+        id: checkUser.id,
+        isAdmin: checkUser.isAdmin,
+      });
+
+      const refresh_token = await generalRefreshToken({
+        id: checkUser.id,
+        isAdmin: checkUser.isAdmin,
+      });
+
+      // ✅ SỬA Ở ĐÂY: TRẢ USER + isAdmin
+      resolve({
+        status: "OK",
+        message: "SUCCESS",
+        access_token,
+        refresh_token,
+        user: {
+          _id: checkUser._id,
+          name: checkUser.name,
+          email: checkUser.email,
+          phone: checkUser.phone,
+          isAdmin: checkUser.isAdmin,
+        },
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+const getAllUser = () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const allUser = await User.find();
+      resolve({
+        status: "OK",
+        message: "Success",
+        data: allUser,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+const getDetailUser = (id) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const user = await User.findOne({
+        _id: id,
+      });
+      if (user === null) {
+        return resolve({
+          status: "ERR",
+          message: "The user is not defined",
+        });
+      }
+
+      resolve({
+        status: "OK",
+        message: "Success",
+        data: user,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+const deleteUser = (id) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const user = await User.findOne({
+        _id: id,
+      });
+      if (user === null) {
+        return resolve({
+          status: "ERR",
+          message: "The user is not defined",
+        });
+      }
+
+      await User.findByIdAndDelete(id);
+      resolve({
+        status: "OK",
+        message: "Delete user success",
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+const updateUser = (id, data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const user = await User.findOne({
+        _id: id,
+      });
+      if (user === null) {
+        return resolve({
+          status: "ERR",
+          message: "The user is not defined",
+        });
+      }
+
+      const updatedUser = await User.findByIdAndUpdate(id, data, {
+        new: true,
+      });
+      resolve({
+        status: "OK",
+        message: "Success",
+        data: updatedUser,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+module.exports = {
+  createUser,
+  loginUser,
+  getAllUser,
+  getDetailUser,
+  deleteUser,
+  updateUser,
+};
